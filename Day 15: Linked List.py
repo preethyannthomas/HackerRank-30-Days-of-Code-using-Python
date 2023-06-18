337de67bddcab7ba5c2a6d5b4ea0cfece72861ac
Preethy Ann Thomas <77205001+preethyannthomas@users.noreply.github.com>
2023-06-18 23:09:50 +0530

    Linked List

---
+++ Day 15: Linked List.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data): 
+    #Complete this method
+        new = Node(data)
+        new.data = data  
+        current = head     
+        if head is None:
+            head = new
+        else:       
+            while current.next is not None:
+                current = current.next
+            current.next = new
+        return head
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head); 	  
